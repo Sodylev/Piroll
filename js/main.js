24baf9b47f4b8565ef98f9809b44ce81f1131fff
rus_respect <rus_respect.98@mail.ru>
2021-01-13 13:25:27 +0700

    add site

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.container__sliders').slick({
+        arrows: false,
+        dots: true,
+        initialSlide: 1,
+        autoplay: true,
+        easing: 'easer',
+        speed: 2000,
+        autoplaySpeed:3000,
+        pauseOnFocus: true,
+    })
+})
+    
